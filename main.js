9c6ba121ec6a20c6a8828a52248934665ce71bf4
_7adi <aaktaditya09@gmail.com>
2024-11-30 03:11:48 +0530

    2nd

---
+++ main.js
@@ -0,0 +1,9 @@
+// JavaScript File: main.js
+
+// Mobile Menu Toggle
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
